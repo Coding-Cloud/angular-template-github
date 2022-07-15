7d34479f5e1f25d4b0d927bee5d25d165c55958b
Rémy Machavoine <remygt@hotmail.fr>
2022-07-15 15:40:17 +0200

    secund init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PommeComponent } from './pomme/pomme.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'pomme', component: PommeComponent},
+  {path: '*', component: AppComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
